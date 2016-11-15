fe1602a70779ac2f44a9ebd59fb68bc4a49c3a5b
Wil Tan <wil@dready.org>
2016-11-15 23:26:32 +1100

    Bump version to 0.2

--- country_currencies/__init__.py
+++ country_currencies/__init__.py
@@ -1,7 +1,7 @@
 from .data import CURRENCIES_BY_COUNTRY_CODE
 
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 def get_by_country(country_code, default=None):
     currencies = CURRENCIES_BY_COUNTRY_CODE.get(country_code)
